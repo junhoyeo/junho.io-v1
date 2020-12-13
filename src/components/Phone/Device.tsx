7a1c9e0b21d5ba87057b543d9a46f74268f4f440
Junho Yeo <hanaro0704@gmail.com>
2020-12-13 18:32:22 +0900

    feat: add buttons for Device

--- src/components/Phone/Device.tsx
+++ src/components/Phone/Device.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Device = () => {
   return (
@@ -9,6 +9,14 @@ const Device = () => {
           <Notch />
         </NotchWrapper>
       </Screen>
+      <LeftButtons>
+        <SilentSwitch />
+        <VolumeUpButton />
+        <VolumeDownButton />
+      </LeftButtons>
+      <RightButtons>
+        <PowerButton />
+      </RightButtons>
     </Bezel>
   );
 };
@@ -16,6 +24,7 @@ const Device = () => {
 export default Device;
 
 const Bezel = styled.div`
+  position: relative;
   background-color: #1a1b1b;
   border-radius: 52px;
   border: 1px solid #3d3d3d;
@@ -78,4 +87,58 @@ const Notch = styled.div`
       #1a1b1b 8px
     );
   }
+`;
+
+const LeftButtons = styled.div`
+  position: absolute;
+  top: 120px;
+  left: -6px;
+`;
+
+const buttonStyle = css`
+  width: 7px;
+  background-color: #2b2b2c;
+  border: 2px solid #0f0f0f;
+`;
+
+const leftButtonStyle = css`
+  ${buttonStyle}
+  border-right: 0;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+`;
+
+const SilentSwitch = styled.div`
+  ${leftButtonStyle}
+  height: 36px;
+`;
+
+const VolumeUpButton = styled.div`
+  ${leftButtonStyle}
+  height: 62px;
+  margin-top: 27px;
+`;
+
+const VolumeDownButton = styled.div`
+  ${leftButtonStyle}
+  height: 62px;
+  margin-top: 7px;
+`;
+
+const RightButtons = styled.div`
+  position: absolute;
+  top: 182px;
+  right: -6px;
+`;
+
+const rightButtonStyle = css`
+  ${buttonStyle}
+  border-left: 0;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+`;
+
+const PowerButton = styled.div`
+  ${rightButtonStyle}
+  height: 101px;
 `;
