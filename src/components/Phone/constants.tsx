9ed872c19abffe05b1105b5167aa821c95fa870d
Junho Yeo <hanaro0704@gmail.com>
2021-01-18 03:18:34 +0900

    post(hipservie): about minimap

--- src/components/Phone/constants.tsx
+++ src/components/Phone/constants.tsx
@@ -4,15 +4,25 @@ export const DEVICE_WIDTH = 413;
 export const DEVICE_HEIGHT = 850;
 
 export const INSTALLED_APPS: IGridItem[] = [
+  // {
+  //   name: 'GitHub',
+  //   icon: require('../../assets/phone/icons/github.webp'),
+  //   color: '#1b2023',
+  //   onClick: () => {
+  //     // setTimeout(() => {
+  //     //   const win = window.open('https://github.com/junhoyeo', '_blank');
+  //     //   win?.open();
+  //     // }, 800);
+  //   },
+  // },
   {
-    name: 'GitHub',
-    icon: require('../../assets/phone/icons/github.webp'),
-    color: '#1b2023',
+    name: '미니맵',
+    icon: require('../../assets/phone/icons/minimap.webp'),
+    color: '#ffffff',
     notifications: 1,
     onClick: () => {
       setTimeout(() => {
-        const win = window.open('https://github.com/junhoyeo', '_blank');
-        win?.open();
+        location.href = '/hipservie/minimap/index.html';
       }, 800);
     },
   },
