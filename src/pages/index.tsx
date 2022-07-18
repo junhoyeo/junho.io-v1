739e562e99a136f14010420671fe863a90bf877b
Junho Yeo <i@junho.io>
2022-07-19 03:33:20 +0900

    Add description in Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,23 @@ import React from 'react';
 import Layout from '../components/Layout';
 
 const IndexPage = () => (
-  <Layout title={<>Hello!</>} description={<>Welcome to my blog.</>}></Layout>
+  <Layout
+    title={
+      <>
+        Hello!
+        <br />
+        I'm Junho Yeo.
+      </>
+    }
+    description={
+      <>
+        Welcome to my site. I'm a 19-yo developer in Seoul, shaping the 2nd/3rd
+        web. Sometimes a designer and dreamer, I just like to build things. Now
+        I'm preparing the infrastructure—insight, followers, and capital— for a
+        bigger dream.
+      </>
+    }
+  ></Layout>
 );
 
 export default IndexPage;
