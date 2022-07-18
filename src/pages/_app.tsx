54bee54847f1649ec41b99d24f9cbf19c383fe0c
Junho Yeo <i@junho.io>
2022-07-18 23:52:33 +0900

    Import web font `Montserrat:wght@90` with `display=block`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React from 'react';
 
 import { GlobalStyle } from '../components/GlobalStyle';
@@ -5,6 +6,19 @@ import { GlobalStyle } from '../components/GlobalStyle';
 function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=block"
+          rel="stylesheet"
+        />
+      </Head>
+
       <GlobalStyle />
       <Component {...pageProps} />
       <div id="portal" />
