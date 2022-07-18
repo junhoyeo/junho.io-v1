f558e16ec9bfda4c128accd47b4004c9364b7721
Junho Yeo <i@junho.io>
2022-07-18 23:34:52 +0900

    Install dependencies for Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
-import Phone from '../components/Phone';
 
 const IndexPage = () => (
   <Layout
